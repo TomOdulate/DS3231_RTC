67d3d873aadbb7a83ef03cec3bce3e9f8b8741ef
TomTheTinkerer <Tom@TomTheTinkerer.com>
2013-10-03 13:54:29 +0100

    Now uses constructor rather than init() method
    Updated comments to show pin definitions of various Arduino boards

--- examples/Basic/Basic.ino
+++ examples/Basic/Basic.ino
@@ -1,9 +1,13 @@
 /* RTC_DS3231 example
  
     Demonstrates basic usage of the RTC_DS3231 library.
-
- 
-   Created  by Tom The Tinkerer http://www.tomthetinkerer.com
+    Created  by Tom The Tinkerer http://www.tomthetinkerer.com
+ 			
+    Board               I2C / TWI pins
+    Uno, Ethernet       A4 (SDA), A5 (SCL)
+    Mega2560            20 (SDA), 21 (SCL)
+    Leonardo             2 (SDA),  3 (SCL)
+    Due                 20 (SDA), 21 (SCL), SDA1, SCL1
  */
 
 #include <RTC_DS3231.h>
@@ -14,8 +18,7 @@ RTC_DS3231 rtc;
 
 void setup()
 {
-    Serial.begin(4800);    	
-    rtc.init();                                      // initialise the RTC    
+    Serial.begin(4800);    	 
     rtc.SetDateTime(DateTime(__DATE__, __TIME__));   // Sets the RTC to the date & time this sketch was compiled
 }
 
